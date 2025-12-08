fabe446f3fa10f05a4572836b5b5768ad81eb7ba
Sam <bau.samuel.contact@gmail.com>
2025-12-08 10:32:53 +0100

    test

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
+require("dotenv").config();
+require("./models/connection");
+
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-=======
-require('dotenv').config();
-require("./models/connection");
-
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
->>>>>>> ad42b0f8f52b95c74656afdd3736c862cecf70e4
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -20,14 +13,10 @@ var recipesRouter = require("./routes/recipes");
 
 var app = express();
 
-<<<<<<< HEAD
-app.use(logger("dev"));
-=======
 const cors = require("cors");
 app.use(cors());
 
-app.use(logger('dev'));
->>>>>>> ad42b0f8f52b95c74656afdd3736c862cecf70e4
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
