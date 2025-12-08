32035ed91da5ae0b573d8eea34b2ae55e1a169f6
Thomas Morini <thomas.morini@icloud.com>
2025-12-08 15:37:02 +0100

    camel case

--- routes/users.js
+++ routes/users.js
@@ -74,7 +74,7 @@ router.post('/signin', (req, res) => {
 router.get('/isConnected/:token', (req, res) => {
   User.findOne({ token: req.params.token }).then(data => {
     if (data) {
-      res.json({ result: true, userInfo : {'firstname':data.firstname, 'lastname':data.lastname, 'restaurantname':data.restaurantName, 'avatar':data.avatar} });
+      res.json({ result: true, userInfo : {'firstname':data.firstname, 'lastname':data.lastname, 'restaurantName':data.restaurantName, 'avatar':data.avatar} });
     } else {
       res.json({ result: false, error: 'User not found' });
     }
