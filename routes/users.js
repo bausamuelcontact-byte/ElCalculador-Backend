a167c3dac147dd027bb8d7d56b6b65224e0c53b5
Thomas Morini <thomas.morini@icloud.com>
2025-12-09 12:13:56 +0100

    faute de frappe dans routes/user.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 require('../models/connection');
 const User = require('../models/users');
-const { checkBody } = require('../modules/checkBody');
+const { checkBody } = require('../modules/checkbody');
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 
